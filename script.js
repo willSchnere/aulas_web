9f057ec0629398af9816c7a081f0f81f1b5fdc95
willSchnere <schnerwill@gmail.com>
2025-05-05 20:30:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var NomeAluno = "";
+var Nota = 0;
+var Nota2 = 0;
+NomeAluno = prompt("Informe um nome:");
+Nota = Number(prompt("Fala sua nota: "));
+Nota2 = Number(prompt("Fala sua segunda nota:"));
+var Media = Nota + Nota2;
+var MediaFinalAluno = Media / 2;
+let raAluno = 0;
+var PrimeiroNome = "Zill";
+var UltimoNome = "Silva";
+const AulasSemestreTotal= 80;
+var Presença = 0;
+var faltas = 0
+console.log("Nome: "+NomeAluno);
+console.log("Media Final: "+MediaFinalAluno);
+console.log("media bim tipo:"+""+Media);
+console.log("essa e minha nota de logica"+raAluno);
+console.log("Nome:"+" "+PrimeiroNome+" "+UltimoNome);
+console.log(1+1);
+
+/*
+* Soma + quando se trata de string possui uma dupla função sendo um operador de soma e de concatenação
+*/ 
