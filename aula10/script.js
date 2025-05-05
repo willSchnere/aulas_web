649f3e53644b9a635d4e0c7f4269bf7ab8553dbb
willSchnere <schnerwill@gmail.com>
2025-05-05 19:31:16 -0300

    Add files via upload

---
+++ aula10/script.js
@@ -0,0 +1,10 @@
+
+var NomeAluno = "William";
+var Nota = 6.5;
+var Nota2 = 8.5;
+var Media = Nota + Nota2;
+var MediaFinal = Media / 2 
+console.log("Nome",NomeAluno);
+console.log("Media Final",MediaFinal);
+console.log(Media);
+console.log("media bim tipo:",typeof(Media));
