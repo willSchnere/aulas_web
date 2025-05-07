f7d1779c81a035df2566eeffd6b1145262bef287
willSchnere <schnerwill@gmail.com>
2025-05-06 22:11:20 -0300

    Add files via upload

---
+++ Aula13/function.js
@@ -0,0 +1,26 @@
+/*
+    function Nomefuncao(){
+    
+
+    }
+*/
+function CalcularMedia() {
+    var NomeAluno = "henrique";
+    var Nota = 8;
+    var Nota2 = 9;
+    NomeAluno = prompt("Informe um nome:");
+    Nota = Number(prompt("Fala sua nota: "));
+    Nota2 = Number(prompt("Fala sua segunda nota:"));
+    var Media = Nota + Nota2;
+    var MediaFinalAluno = Media / 2;
+    console.log(`A media do Aluno: ${NomeAluno} para as notas ${Nota} e ${Nota2} é ${MediaFinalAluno}`);
+}
+
+function SomarNumeros(){
+    var primeiroNumero = 0;
+    var segundoNumero = 0;
+    var somar = primeiroNumero + segundoNumero;
+    primeiroNumero = prompt("Promeiro numero:");
+    segundoNumero = prompt("Segundo numero: ")
+    
+}
---
+++ Aula13/script.js
@@ -0,0 +1,22 @@
+/*var NomeAluno = "will";
+var Nota = 7;
+var Nota2 = 8;
+NomeAluno = prompt("Informe um nome:");
+Nota = Number(prompt("Fala sua nota: "));
+Nota2 = Number(prompt("Fala sua segunda nota:"));
+var Media = Nota + Nota2;
+var MediaFinalAluno = Media / 2;
+let raAluno = 0;
+var PrimeiroNome = "Zill";
+var UltimoNome = "Silva";
+const AulasSemestreTotal= 80;
+var Presença = 0;
+var faltas = 0
+console.log("Nome: "+NomeAluno);
+console.log("Media Final: "+MediaFinalAluno);
+console.log("media bim tipo:"+""+Media);
+console.log("essa e minha nota de logica"+raAluno);
+console.log("Nome:"+" "+PrimeiroNome+" "+UltimoNome);
+console.log(1+1);
+console.log("A media do Aluno:"+NomeAluno+" "+"para as notas:"+" "+Nota+" "+"e"+" "+Nota2+" "+" "+"é"+" "+MediaFinalAluno);
+console.log(`A media do Aluno: ${NomeAluno} para as notas ${Nota} e ${Nota2} é ${MediaFinalAluno}`);*/
