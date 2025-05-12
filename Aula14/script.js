6890a95adaa931b3bbb95ff428463ca6edfd1912
willSchnere <schnerwill@gmail.com>
2025-05-12 20:29:47 -0300

    Add files via upload

---
+++ Aula14/script.js
@@ -0,0 +1,27 @@
+function EscreverOlaUsuario(nomeUsuario){
+    console.log(`Ola Usuario da UniBrasil ${nomeUsuario}`);
+}
+function Somar(valor1,valor2){
+    const result = valor1 + valor2;
+    return result;
+}
+function Subtrair(valor1,valor2){
+    const result = valor1 - valor2;
+    return result;
+}
+function Multiplicar(valor1,valor2){
+    const result = valor1 * valor2;
+    return result;
+}
+function Dividir(valor1,valor2){
+    const result = valor1 / valor2;
+    return result;
+}
+/*function PedirValor(){
+    let solicit1 =0;
+    let solicit2=0;
+    solicit1 = Number(prompt("Valor 1:"));
+    solicit2 = Number(prompt("Valor 2:"));
+    const result = SomarNumeros(solicit1,solicit2);
+    console.log(`Somar ${solicit1} -${solicit2} : ${result}`);
+}*/
