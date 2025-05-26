2c4ebbc0a38a3743ada6d4320fc0bfd67df831d1
willSchnere <schnerwill@gmail.com>
2025-05-26 20:27:05 -0300

    Add files via upload

---
+++ Aula17/script.js
@@ -0,0 +1,29 @@
+let numeros = [10,"Testando",30.50,"Funcionou"];
+/*console.log(`#0: ${numeros[0]}`);
+console.log(`#1: ${numeros[1]}`);
+console.log(`#2: ${numeros[2]}`);
+console.log(`#3: ${numeros[3]}`);
+*/
+if(numeros [3] == undefined){
+
+}
+/*
+Indice: e a posição do item dentro do array
+*/
+let idx = 0;
+// enquanto o indice for menor que o ta,anho
+while(idx < numeros.length){
+console.log(`#${idx}: ${numeros[idx]}`);
+idx += 1;
+}
+
+
+let numInicial = 0;
+let numFinal = 1000;
+
+while(numInicial < numFinal){
+    console.log(`# ${numInicial}`);
+    numInicial++;
+}
+// idx = idx +
+// idx += 1
